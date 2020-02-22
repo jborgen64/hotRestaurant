94f9f4a7c8d6e9a1ff2dd7b40ad91a987445a37e
Code Master <jeffbell.kc@gmail.com>
2020-02-22 11:05:01 -0600

    made index.js change

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const res = {
   customerEmail: '',
   customerID: '',
 }
+
+const getTables = function () {
+  return $.ajax({
+    url: '/api/tables',
+    method: 'GET',
+  });
+};
+
+// A function for saving a note to the db
+const saveRes = function (res) {
+  return $.ajax({
+    url: '/api/tables',
+    data: res,
+    method: 'POST',
+  });
+};
