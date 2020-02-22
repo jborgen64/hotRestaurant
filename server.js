280daa1472b06f0f731cd22e350a6c93085eaf4c
Michael Bowman <micbowman25@gmail.com>
2020-02-22 11:21:54 -0600

    table route

--- server.js
+++ server.js
@@ -6,12 +6,21 @@ const path = require('path');
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const tables = [];
+const waiting = [];
+
+const res = {
+  customerName: '',
+  phoneNumber: '',
+  customerEmail: '',
+  customerID: '',
+}
 
 // Routes
 // =============================================================
@@ -21,7 +30,28 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'main.html'));
 });
 
+app.get("/add", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
 
+app.get('/api/tables', (req, res) => {
+  return tables;
+});
+
+app.post('/api/tables', (req, res) => {
+  res.sendFile(path.join(__dirname, 'tables.html'));
+  const data = req.body
+
+  if (tables.length < 6) {
+    res.push(data);
+  }
+
+  else {
+    wating.push(data);
+  }
+  
+  
+});
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, () => {
